8f2d779cc8ab66a2f3134dac03d0b85748291324
Sattvik Palta <sattvikpalta@gmail.com>
2026-04-16 22:07:07 -0400

    feat(client): set toast duration to 3 seconds

--- client/src/components/ui/toaster.tsx
+++ client/src/components/ui/toaster.tsx
@@ -4,6 +4,7 @@ function Toaster() {
   return (
     <SonnerToaster
       position="top-right"
+      duration={3000}
       toastOptions={{
         className: "bg-popover text-popover-foreground border-border",
       }}
